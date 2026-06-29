33f79498ab0823e63c898fcae53d6d91119f72e2
Pontus Rydin <pontus.rydin@gmail.com>
2026-06-29 15:19:41 -0700

    WIP

--- k8s-vm/delete-all.sh
+++ k8s-vm/delete-all.sh
@@ -8,7 +8,6 @@ DNS_VIP=10.1.8.133
 DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 
 for f in \
-    05-secreets.yaml \
     10-postgres-vm.yaml \
     20-backend-vm.yaml \
     30-frontend.yaml \
