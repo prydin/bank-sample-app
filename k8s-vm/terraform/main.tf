2d834ddf1a3379539906fb1b45942cce49d1bd14
Pontus Rydin <pontus@rydin.nu>
2026-06-29 15:37:46 -0400

    WIP

--- k8s-vm/terraform/main.tf
+++ k8s-vm/terraform/main.tf
@@ -1,9 +1,14 @@
+locals {
+  postgres_vm = [for d in split("\n---\n", file("${path.module}/../10-postgres-vm.yaml")) : yamldecode(d) if try(yamldecode(d).kind, "") == "VirtualMachine"][0]
+  backend_vm  = [for d in split("\n---\n", file("${path.module}/../20-backend-vm.yaml")) : yamldecode(d) if try(yamldecode(d).kind, "") == "VirtualMachine"][0]
+}
+
 resource "kubernetes_manifest" "postgres" {
-  manifest = merge(yamldecode(file("${path.module}/../10-postgres-vm.yaml")).metadata, { namespace = var.namespace})
+  manifest = merge(local.postgres_vm, { metadata = merge(local.postgres_vm.metadata, { namespace = var.namespace }) })
 }
 
 resource "kubernetes_manifest" "backend" {
-  manifest = merge(yamldecode(file("${path.module}/../20-backend-vm.yaml")).metadata, { namespace = var.namespace})
+  manifest = merge(local.backend_vm, { metadata = merge(local.backend_vm.metadata, { namespace = var.namespace }) })
 }
 
 resource "kubernetes_manifest" "frontend" {
