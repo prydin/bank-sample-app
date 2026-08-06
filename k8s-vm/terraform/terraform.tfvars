388dd750b003b828829e17d479856a61f8bab2ee
Pontus Rydin <pontus.rydin@gmail.com>
2026-08-06 04:30:32 -0700

    WIP

--- k8s-vm/terraform/terraform.tfvars
+++ k8s-vm/terraform/terraform.tfvars
@@ -1,5 +1,8 @@
 # Copy to terraform.tfvars and fill in your values.
 
 # VM Operator (Supervisor) namespace to deploy into.
-namespace = "bank-vm-r3n8y"
-vm_count = 2
+namespace = "bank-yhjvg"
+region_name = "us-east-a"
+org_name = "Acme-East-A"
+vm_count = 4
+vcfa_url = "https://auto-a.site-a.vcf.lab"
--- k8s-vm/terraform/variables.tf
+++ k8s-vm/terraform/variables.tf
@@ -3,6 +3,21 @@ variable "namespace" {
   type        = string
 }
 
+variable "org_name" {
+  description = "VCFA Organization name"
+  type        = string
+}
+
+variable "region_name" {
+  description = "The region this deployment will be placed in"
+  type        = string
+}
+
+variable "vcfa_url" {
+  description = "VCFA URL"
+  type        = string
+}
+
 variable "vm_count" {
   description = "Number of frontend VMs to create. Named frontend-01, frontend-02, etc."
   type        = number
